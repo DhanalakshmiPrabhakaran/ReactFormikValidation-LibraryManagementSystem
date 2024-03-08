7fab92925cbdf7102d34b87b949e00fa9664d1bd
DhanalakshmiPrabhakaran <151752769+DhanalakshmiPrabhakaran@users.noreply.github.com>
2024-03-09 02:38:28 +0530

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+        <div className="container" >
+      <div className="row ">
+        <div className="heading text-white p-5 mt-3 mb-3 rounded-4 ">
+          <h1 className="text-center mt-3 ">Welcome to <span style={{color:"#f9bc98"}}><i>Dhanalakshmi Library</i></span> Management System</h1>
+        </div>
+        </div>
+        </div>
+    </>
+  )
+}
+
+export default Home
